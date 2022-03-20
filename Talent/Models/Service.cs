7afcb8eb45b50d6c7a81fc09474fe03726a3aeab
linkstu <55122525+linkstu@users.noreply.github.com>
2022-03-20 17:01:21 +0800

    Update Service.cs

--- Talent/Models/Service.cs
+++ Talent/Models/Service.cs
@@ -6,35 +6,6 @@ using WalkingTec.Mvvm.Core;
 namespace Talent.Models
 {
     /// <summary>
-    /// 服务类别
-    /// </summary>
-    [Table("ServiceCategories")]
-    public class ServiceCategory : BasePoco
-    {
-        [Display(Name = "服务名称"), StringLength(100), Required]
-        public string Name { get; set; }
-
-
-        [Display(Name = "服务性质"), StringLength(100), Required]
-        public EnumServiceType Type { get; set; }
-
-        [Display(Name ="年度限额（元）"), Required]
-        public decimal LimitedValuation { get; set; }
-
-        [Display(Name = "年度限额（次）"), Required]
-        public int LimitedCount { get; set; }
-
-        [Display(Name ="描述"), StringLength(500)]
-        public string Description { get; set; }
-
-        [Display(Name = "启用"), Required]
-        public bool IsEnabled { get; set; }
-
-        [Display(Name = "排序"), Required]
-        public int SortIndex { get; set; }
-    }
-
-    /// <summary>
     /// 服务场所
     /// </summary>
     [Table("Services")]
