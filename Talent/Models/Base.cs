55273f8ce5ef90e2e19d9cb13956d2f8f6de696e
linkstu <55122525+linkstu@users.noreply.github.com>
2022-03-20 16:44:26 +0800

    Create Base.cs
    系统基础 放置于此

---
+++ Talent/Models/Base.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Talent.Models
+{
+    /*
+     * 系统模块
+     */
+
+    /// <summary>
+    /// 行业类别
+    /// </summary>
+    [Table("IndustryCategories")]
+    public class IndustryCategory
+    {
+        [Display(Name ="类别"), StringLength(20), Required]
+        public string Category { get; set; }
+
+        [Display(Name = "启用"), Required]
+        public bool IsEnabled { get; set; }
+
+        [Display(Name ="排序"), Required]
+        public int SortIndex { get; set; }
+    }
+}
