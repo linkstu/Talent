45bdb93389d3e1f98ee1bb09d984c45d68d9de9e
linkstu <55122525+linkstu@users.noreply.github.com>
2022-03-20 17:01:19 +0800

    Update Base.cs

--- Talent/Models/Base.cs
+++ Talent/Models/Base.cs
@@ -46,4 +46,33 @@ namespace Talent.Models
         public bool IsEnabled { get; set; }
 
     }
+
+    /// <summary>
+    /// 服务类别
+    /// </summary>
+    [Table("ServiceCategories")]
+    public class ServiceCategory : BasePoco
+    {
+        [Display(Name = "服务名称"), StringLength(100), Required]
+        public string Name { get; set; }
+
+
+        [Display(Name = "服务性质"), StringLength(100), Required]
+        public EnumServiceType Type { get; set; }
+
+        [Display(Name = "年度限额（元）"), Required]
+        public decimal LimitedValuation { get; set; }
+
+        [Display(Name = "年度限额（次）"), Required]
+        public int LimitedCount { get; set; }
+
+        [Display(Name = "描述"), StringLength(500)]
+        public string Description { get; set; }
+
+        [Display(Name = "启用"), Required]
+        public bool IsEnabled { get; set; }
+
+        [Display(Name = "排序"), Required]
+        public int SortIndex { get; set; }
+    }
 }
