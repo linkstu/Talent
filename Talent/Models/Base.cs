1cec88c47c2f3ff4fad14db39e3ac9674b833390
linkstu <55122525+linkstu@users.noreply.github.com>
2022-03-20 16:49:40 +0800

    Update Base.cs
    增加导航图片

--- Talent/Models/Base.cs
+++ Talent/Models/Base.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using WalkingTec.Mvvm.Core;
 
 namespace Talent.Models
 {
@@ -11,7 +12,7 @@ namespace Talent.Models
     /// 行业类别
     /// </summary>
     [Table("IndustryCategories")]
-    public class IndustryCategory
+    public class IndustryCategory : BasePoco
     {
         [Display(Name ="类别"), StringLength(20), Required]
         public string Category { get; set; }
@@ -21,5 +22,28 @@ namespace Talent.Models
 
         [Display(Name ="排序"), Required]
         public int SortIndex { get; set; }
+    }
+
+    /// <summary>
+    /// 导航图片
+    /// </summary>
+    [Table("Banners")]
+    public class Banner : BasePoco
+    {
+        [Display(Name = "标题"), StringLength(100), Required]
+        public string Title { get; set; }
+
+        [Display(Name = "跳转地址"), StringLength(255)]
+        public string Url { get; set; }
+
+        [Display(Name = "图片"), Required]
+        public FileAttachment Image { get; set; }
+
+        [Display(Name = "排序"), Required]
+        public int SortIndex { get; set; }
+
+        [Display(Name = "启用"), Required]
+        public bool IsEnabled { get; set; }
+
     }
 }
